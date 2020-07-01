65678c431d9f0821daf500156924d3f19c9c184b
Theresa <tli2@cca.edu>
2020-06-30 20:07:30 -0700

    Implement BatchPage

--- src/Components/App.js
+++ src/Components/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import '../App.css';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
+import BatchPage from './Batch/BatchPage';
+
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Switch>
-
+          <Route path="/batch" component={BatchPage} />
         </Switch>
       </BrowserRouter>
     </div>
---
+++ src/Components/Batch/BatchPage.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+
+function BatchPage() {
+  const [batch, setBatch] = useState({/*TODO: Set up default data*/});
+
+  //TODO: Get request for batch information and setBatch
+
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default BatchPage;
