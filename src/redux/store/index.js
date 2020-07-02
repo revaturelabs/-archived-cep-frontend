41da2cbf22aa55207f44d250605d4961952505e2
Erhunosa Osagie-Amayo <erhunosaosagie-amayo@erhunosas-air.home>
2020-07-02 14:37:52 -0700

    added logging and testing

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,7 +1,19 @@
-import { createStore } from 'redux';
+import { createStore, compose, applyMiddleware } from "redux";
+import logger from 'redux-logger';
 //import { rootReducer } from '../ducks/index.js';
 import mainReducer from "../reducers/index";
 
-const store = createStore(mainReducer);
+const initialState = {};
+
+const store = createStore(
+  mainReducer, initialState,
+  compose(
+    applyMiddleware(logger), window.__REDUX_DEVTOOLS_EXTENSION__
+    ? window.__REDUX_DEVTOOLS_EXTENSION__()
+    : f => f
+  )
+);
 
 export default store;
+
+
