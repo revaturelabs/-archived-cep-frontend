50f4951c7afc91d02155616505ede10fada57a98
Soksivateara Eng <tearaeng@yahoo.com>
2020-07-03 13:05:49 -0500

    LoginPage will show both the login component & pic

--- src/Components/login/LoginPage.jsx
+++ src/Components/login/LoginPage.jsx
@@ -1,95 +1,35 @@
-import React, { useState} from "react";
-import { useDispatch } from "react-redux";
-import { dispatchToken } from "../../redux/actions/userAction";
-//import JWTD from "jwt-decode";
-import Axios from "axios";
+import React from 'react'
+import { Container, Grid, makeStyles, CssBaseline } from '@material-ui/core'
+import Login from './Login';
+//used solely for styling 
+const useStyles = makeStyles((theme) => ({
+    image: {
+        backgroundImage: 'url(https://source.unsplash.com/random)',
+        backgroundRepeat: 'no-repeat',
+        backgroundColor:
+          theme.palette.type === 'light' ? theme.palette.grey[50] : theme.palette.grey[900],
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+    },
+    right: {
+        backgroundColor: 'Lime'
+    },
+}));
 
-export default function LoginPage(props) {
-  const [userCredentials, setCredentials] = useState({
-    username: "",
-    password: "",
-  });
+export default function LoginPage(){
+    const styles = useStyles();
 
-  const [role, setRole] = useState("");
-  const dispatch = useDispatch();
-
-  // function getRole(token) {
-  //   //Getting the role of the user to conditionally re-route to either 
-  //   // admin page or my_batches page
-  //   const username = JWTD(token).sub;
-  //   Axios.get("http://13.58.157.19:8081/role/" + username, {
-  //     headers: {
-  //       Authorization: `Bearer ${token}`,
-  //     },
-  //   })
-  //     .then((result) => {
-  //       setRole(result.data);
-  //     })
-  //     .catch((err) => console.log("error username:" + err));
-  // }
-
-  function handleChange(event) {
-    setCredentials({
-      ...userCredentials,
-      [event.target.name]: event.target.value,
-    });
-  }
-
-  function handleSubmit(event) {
-    // event.preventDefault();
-
-    // Authorize the user
-    // fetch("http://13.58.157.19:8081/authenticate", {
-    //   method: "POST",
-    //   body: JSON.stringify({
-    //     username: userCredentials.username,
-    //     password: userCredentials.password,
-    //   }),
-    //   headers: {
-    //     "Content-Type": "application/json",
-    //   },
-    // })
-    //   .then((res) => res.json())
-    //   .then((res) => {
-    //     console.log(res);
-    //     dispatch(dispatchToken(res.jwt)); //send jwt to store
-    //     getRole(res.jwt); 
-    //     //If statement to redirect based on user and admin
-    //     if (role !== "admin"){
-    //         props.history.push("/my_batches");
-    //     }
-    //     props.history.push("/admin");
-    //   })
-    //   .catch((error) => {
-    //     console.log(error);
-    //   });
-  }
-
-  return (
-    <div>
-       <form onSubmit={handleSubmit}> 
-        <br />
-          <label htmlFor="username" onChange={handleChange}>User Name:
-          <input
-            type="text"
-            placeholder="Enter user name"
-            name="username"
-            required
-          />
-          </label>
-
-          <label htmlFor="password" onChange={handleChange}>Password:
-          <input
-            type="password"
-            placeholder="Enter password"
-            name="password"
-            required
-          />
-          </label>
-        <button type="submit">
-          Log In
-        </button>
-      </form>  
-    </div>
-  );
+    return(
+        <div>
+            <Container maxWidth="sm">
+                <CssBaseline />
+                <Grid container>
+                <Grid item xs={false} sm={4} md={7} className={styles.image} />
+                    <Grid item xs={12} sm={8} md={5} elevation={6} square>
+                        <Login />
+                    </Grid>
+                </Grid>
+            </Container>
+        </div>
+    )
 }
