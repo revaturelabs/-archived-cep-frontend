15b630c7b1a7562a23137ac9c2cbd21adcd14056
Soksivateara Eng <tearaeng@yahoo.com>
2020-07-05 20:39:58 -0500

    Added states for url link

--- src/redux/ducks/index.js
+++ src/redux/ducks/index.js
@@ -1,19 +1,22 @@
 export const actionTypes = {
   ADD_TOKEN: 'ADD_TOKEN',
   ADD_USERID: 'ADD_USERID',
-  ADD_ROLE: 'ADD_ROLE'
+  ADD_ROLE: 'ADD_ROLE',
+  ADD_LINK: 'ADD_LINK',
 };
 
 export const actions = {
   addToken: token => ({ type: actionTypes.ADD_TOKEN, payload: token }),
   addUserID: userID => ({ type: actionTypes.ADD_USERID, payload: userID }),
   addRole: role => ({ type: actionTypes.ADD_ROLE, payload: role }),
+  addLink: link => ({ type: actionTypes.ADD_LINK, payload: link }),
 };
 
 const initialState = {
   token: null,
   userID: null,
   role: null,
+  link: '/',
 };
 
 export const rootReducer = (state = initialState, action) => {
@@ -26,6 +29,9 @@ export const rootReducer = (state = initialState, action) => {
       return state;
     case 'ADD_ROLE':
       state.role = action.payload;
+      return state;
+    case 'ADD_LINK':
+      state.link = action.payload;
       return state;
     default:
       return state;
