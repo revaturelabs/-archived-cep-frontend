4b1579b63d786ecadd5589adf6c5bc6ed9c49680
Theresa <tli2@cca.edu>
2020-07-07 16:54:17 -0700

    Get batches and pass information down to batch nav

--- src/Components/Batch/BatchPage.js
+++ src/Components/Batch/BatchPage.js
@@ -1,27 +1,38 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import GetBatchDetails from "./GetBatches";
 import SimpleModal from '../Common/Modal';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
+import Axios from 'axios';
 
 function BatchPage() {
-  const [batch, setBatch] = useState({/*TODO: Set up default data*/});
+  const [batches, setBatches] = useState([]);
 
   //TODO: Get request for batch information and setBatch
+  useEffect(() => {
+    Axios.get("http://localhost:8080/UB/batchesbyuser", {
+      params: {
+        userId: 1,
+      },
+    })
+      .then((result) => {
+        setBatches(result.data);
+      })
+      .catch((err) => console.log("error batch:" + err));
+  }, []);
 
   return (
     <Grid container>
       <Grid item sm={2}>
         <Paper>
           <h2>Batches Available</h2>
-          <GetBatchDetails/>
+          <GetBatchDetails batches={batches}/>
         </Paper>
 
       </Grid>
       <Grid item sm={10}>
         <SimpleModal />
       </Grid>
-  
     </Grid>
   );
 }
--- src/Components/Batch/GetBatches.js
+++ src/Components/Batch/GetBatches.js
@@ -3,41 +3,20 @@ import axios from "axios";
 import { useState } from "react";
 import { Button } from "@material-ui/core";
 
-export default function GetBatchDetails() {
-  const [details, setDetails] = useState([]);
-
-    axios
-      .get("http://localhost:8080/UB/all", {
-        headers: {
-          "Content-Type": "application/json",
-        },
-      })
-      .then(function (response) {
-        setDetails(response.data);
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-  console.log(details);
+export default function GetBatchDetails({ batches }) {
   return (
     <>
       {/* <Button variant="contained" color="primary" onClick={handleSubmit}>
         Display
       </Button> */}
 
-      {details.map((detail) => (
-        <div key={detail.id}>
+      {batches.map((detail) => (
+        <div key={detail.batchId}>
           <Button variant='outlined'>
           <table>
-          
-          <tr style={{fontSize:'13px'}}>Batch ID: {detail.batchId}</tr>
-          {/* <tr style={{fontSize:'13px'}}>Batch Name: {detail.name}</tr> */}
-          {/* <tr style={{fontSize:'13px'}}>Batch Skill: {detail.skill}</tr>
-          <tr style={{fontSize:'13px'}}>Batch Start Date: {detail.startDate}</tr>
-          <tr style={{fontSize:'13px'}}>Batch End Date: {detail.endDate}</tr>
-          <tr style={{fontSize:'13px'}}>Batch Location: {detail.location}</tr>
-          <tr style={{fontSize:'13px'}}>Current Week: {detail.currentWeek}</tr>
-          <tr style={{fontSize:'13px'}}>Progress: {(detail.currentWeek * 100) / 10}%</tr> */}
+            <tbody>
+              <tr style={{fontSize:'13px'}}><td>Batch ID: {detail.batchId}</td></tr>
+            </tbody>
           </table>
           </Button>
           <br />
