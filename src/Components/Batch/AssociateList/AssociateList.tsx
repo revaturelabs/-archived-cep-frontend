a347280b36eb490f12aba66eca3936920d44b312
Jacob Rollins <jacob.rollins1247@gmail.com>
2020-07-30 13:37:33 -0600

    updated types on batch components

--- src/Components/Batch/AssociateList/AssociateList.tsx
+++ src/Components/Batch/AssociateList/AssociateList.tsx
@@ -1,18 +1,28 @@
-import React, { ReactElement } from 'react';
+import React, { ReactElement, ObjectHTMLAttributes } from 'react';
 
 import AssociateListItem from './AssociateListItem';
 import { useSelector } from 'react-redux';
 import { Grid } from '@material-ui/core';
 
+interface batchINF
+{
+  batchId: Number;
+}
+interface associateINF
+{
+  email: string;
+  firstName: string;
+  lastName: string;
+}
 function AssociateList(props: any): ReactElement {
 
   const {batch} = props;
 
-  const associates: any = useSelector((state: any) => state.batchReducer.associateAssignments);
+  const associates: associateINF[] = useSelector((state: any) => state.batchReducer.associateAssignments);
 
   return (
     <Grid container direction="row" spacing={3}>
-      {associates.map((associate: any) => <AssociateListItem key={batch.batchId} batch={batch.batchId} associate={associate} />)}
+      {associates.map((associate: associateINF) => <AssociateListItem key={batch.batchId} batch={batch.batchId} associate={associate} />)}
     </Grid>
   );
 }
