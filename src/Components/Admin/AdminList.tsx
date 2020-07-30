f4127e11fbf7b4c101e9df4d0495508709357f7c
Simon Nardos <simonnardos@MacBook-Pro.local>
2020-07-30 08:55:11 -0600

    Added return types to functions in admin components

--- src/Components/Admin/AdminList.tsx
+++ src/Components/Admin/AdminList.tsx
@@ -1,20 +1,20 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, ReactElement } from "react";
 import AdminItem from "./AdminItem";
 import axios from "axios";
 import { useSelector } from "react-redux";
 import apiBasePath from "../../apiBasePath";
 
 //Show a list of requests
-export default function AdminList() {
+export default function AdminList():ReactElement {
   const token = useSelector((state: any) => state.credReducer.token);
   const [realData, setRealData] = useState([]);
 
-  function changeRealData(data: any) {
+  function changeRealData(data: any):void {
     setRealData(data);
   }
 
   //Make an axios call to display the list of requests
-  useEffect(() => {
+  useEffect(():void => {
     axios
       .get(
         apiBasePath + "/interventions",
