a2691317342448ac1d83abcec1c38c4391053cf8
Armondo Gomez <armondolgomez@gmail.com>
2020-08-07 16:49:17 -0500

    According menu

--- src/Components/Common/Calendar/Event.jsx
+++ src/Components/Common/Calendar/Event.jsx
@@ -1,17 +1,26 @@
-import React from 'react';
+import React , {useState}from 'react';
+import moment from 'moment';
+import "./event.css";
 function Event(props) {
-/*     batchId: 1,
-    userId: 2,
-    startTime: 3,
-    endTime: 4,
-    isAllDay: 5,
-    status: "Pending",
-    requestType: 7,
-    description: 8 */
+    /*     batchId: 1,
+        userId: 2,
+        startTime: 3,
+        endTime: 4,
+        isAllDay: 5,
+        status: "Pending",
+        requestType: 7,
+        description: 8 */
+        const [displayBlock, setDisplay] = useState(false);
     return (
-        <div style={{backgroundColor:"Yellow", fontSize: 15 }} onClick={() => alert(props.eventItem.description)}>
-    User: {props.eventItem.userId} :: Batch: {props.eventItem.batchId} :: Start: {props.eventItem.startTime} :: End: {props.eventItem.endTime} :: AllDay: {props.eventItem.isAllDay} :: Type: {props.eventItem.requestType}
-    </div>
+        <div style={{ backgroundColor: "Yellow", fontSize: 15 }} >
+            <button onClick={() => setDisplay(!displayBlock)} class="accordion">{"UserId: " + props.eventItem.userId + " BatchID " + props.eventItem.batchId }</button>
+            <div class="panel" style={{display: displayBlock ? "block":"none"}}>
+                <p>Start: {moment(props.eventItem.startTime).format("hh:mm a")}</p>
+                <p>End: {moment(props.eventItem.endTime).format("hh:mm a")}</p>
+                <p>AllDay: {props.eventItem.isAllDay.toString()}</p>
+                <p>Type: {props.eventItem.requestType}</p>
+            </div>
+        </div>
 
     )
 }
